86a081c205af264a3b35665ae308f76772e7b4b5
Sumit Mehrotra <srvacct@gmail.com>
2014-02-20 16:20:25 -0800

    Adding the project v1.0

---
+++ testapp/src/calc.rb
@@ -0,0 +1,5 @@
+class Calc 
+    def add(x,y)
+	return x+y
+    end
+end
---
+++ testapp/test/testcalc.rb
@@ -0,0 +1,9 @@
+require 'test/unit'
+require 'src/calc'
+class TestAdd < Test::Unit::TestCase
+    def test_add
+    	calc = Calc.new
+    	expected = calc.add 3,2
+    	assert_equal expected, 5
+    end
+end
