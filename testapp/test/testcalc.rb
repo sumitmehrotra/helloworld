4141e8eca640e22f66f541aee34f506ca9957426
Sumit Mehrotra <srvacct@gmail.com>
2014-03-04 16:06:41 -0800

    Fixing path references to require the calc library in tests

--- testapp/test/testcalc.rb
+++ testapp/test/testcalc.rb
@@ -1,6 +1,16 @@
 # version 11.0
+=begin
+$LOAD_PATH.each do |path|
+	puts "LOAD PATH: " + path
+end
+$LOAD_PATH << File.dirname(__FILE__)
+$LOAD_PATH.each do |path|
+	puts "LOAD PATH: " + path
+end
+puts "__FILE__ dirname" + File.dirname(__FILE__)
+=end
 require 'test/unit'
-require 'src/calc'
+require File.join File.dirname(__FILE__), '../src/calc'
 class TestAdd < Test::Unit::TestCase
     def test_add
     	calc = Calc.new
